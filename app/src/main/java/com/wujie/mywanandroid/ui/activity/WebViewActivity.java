acaf4f0b18df7dd116bb43e710dc959bcbfc8231
HuangBin <1085622791@qq.com>
2019-01-26 15:27:20 +0800

    WebView修改

--- app/src/main/java/com/wujie/mywanandroid/ui/activity/WebViewActivity.java
+++ app/src/main/java/com/wujie/mywanandroid/ui/activity/WebViewActivity.java
@@ -1,8 +1,14 @@
 package com.wujie.mywanandroid.ui.activity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.wujie.commonmoudle.base.BaseActivity;
@@ -11,6 +17,7 @@ import com.wujie.commonmoudle.utils.ARouterUtils;
 import com.wujie.mywanandroid.R;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Time：2019/1/14 0014 上午 10:08
@@ -19,8 +26,12 @@ import butterknife.BindView;
  **/
 @Route(path = ARouterUtils.WebViewPath)
 public class WebViewActivity extends BaseActivity {
-    @BindView(R.id.web_view)
+//    @BindView(R.id.web_view)
     WebView mWebView;
+    @BindView(R.id.progress_bar)
+    ProgressBar mProgressBar;
+    @BindView(R.id.fl_container)
+    FrameLayout mFlContainer;
 
     @Override
     protected int getLayoutId() {
@@ -35,9 +46,60 @@ public class WebViewActivity extends BaseActivity {
     @Override
     protected void init() {
         String webUrl = getIntent().getStringExtra("WebUrl");
+        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        mWebView = new WebView(getApplicationContext());
+        mWebView.setLayoutParams(lp);
+        mFlContainer.addView(mWebView, 0);
         WebSettings settings = mWebView.getSettings();
         settings.setJavaScriptEnabled(true);
         mWebView.loadUrl(webUrl);
+        mProgressBar.setMax(100);
+
+        mWebView.setWebChromeClient(new WebChromeClient() {
+
+            //获取网页的标题
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+//                Toast.makeText(mContext, "" + title, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                mProgressBar.setProgress(newProgress);
+                if (newProgress == 100) {
+                    mProgressBar.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mWebView != null) {
+            mWebView.loadDataWithBaseURL(null, "", "text/html", "utf-8", null);
+            mWebView.clearHistory();
+            ((ViewGroup) mWebView.getParent()).removeView(mWebView);
+            mWebView.destroy();
+            mWebView = null;
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
 }
